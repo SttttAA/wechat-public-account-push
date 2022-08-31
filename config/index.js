505470b3579317846bf74b56d964cad553a61425
SttttAA <112543662+SttttAA@users.noreply.github.com>
2022-08-31 16:32:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,16 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wxb67ceaf474523715",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "e839fa7e084145c45cf476824ae44972",
 
     // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
-
-    // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    TEMPLATE_ID: "OvYdlE2VvIjhSc_6pVVM_xOYmVNDkRLoIOCil6JWPCE",
 
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     // [{
@@ -25,7 +22,7 @@ export const config = {
     //   id: "wxafasdasdd-zxfvsdfd"
     // }]
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
+      {name: '哈喽', id: "oK0oj57woJO2F6gdUc1gmgtHklfw"}, 
       {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
       {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
       {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
@@ -42,9 +39,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "兰州",
     // 所在城市
-    CITY: "惠州",
+    CITY: "七里河",
 
     /** 重要节日相关 */
 
@@ -53,10 +50,10 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
-      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
-      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
+      {"type": "生日", "小宝": "朋友", "year": "1996", "date": "01-01"},
+      {"type": "节日", "小宝": "结婚纪念日", "year": "2020", "date": "01-04"},
+      {"type": "生日", "小宝": "李四", "year": "1996", "date": "08-29"},
+      {"type": "节日", "小宝": "被搭讪纪念日", "year": "2021", "date": "08-29"},
     ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
